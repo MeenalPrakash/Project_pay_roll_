6464908e8e93c77166416532b0733bb1f84b5891
Meenal Prakash <meenalprakash03@gmail.com>
2022-01-05 18:34:06 +0530

    dbconnected

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,15 +1,27 @@
-const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://Pratham123:PrathamMeenal@cluster0.pd1xs.mongodb.net/test";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log("succesfull");
-  // perform actions on the collection object
-  client.close();
-});
+var MongoClient= require('mongodb').MongoClient;
 
-db.USER.insert({
-    'user': 'Harry',
-    'pass': 'JavaScript',
-    
- })
+// or as an es module:
+// import { MongoClient } from 'mongodb'
+// Connection URL
+const url = 'mongodb+srv://Pratham123:PrathamMeenal@cluster0.pd1xs.mongodb.net/PAYROLL?retryWrites=true&w=majority';
+const client = new MongoClient(url);
+// Database Name
+const dbName = 'PAYROLL';
+
+async function main() {
+    // Use connect method to connect to the server
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db(dbName);
+    const collection = db.collection('USER');
+    const insertResult = await collection.insertMany([{ username: 'meenal' }, { lang: 'meenal2123' }]);
+    console.log('Inserted documents =>', insertResult);
+
+
+    // the following code examples can be pasted here...
+    return 'done.';
+}
+main()
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close());
